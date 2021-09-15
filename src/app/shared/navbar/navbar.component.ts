71032d39a36e04fb79bcaa49d45e3ba62471303d
Simón Cebrero <62322984+simoncebrero095@users.noreply.github.com>
2021-09-15 14:36:40 -0300

    narbar en modo responsive

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -11,5 +11,14 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+// --------------------------------------------------------------traducion a DOM en typescript 
+//   addEventListener('DOMContentLoad', () => {
+//       const btn_menu = document.querySelector('.btn_menu')
+//       if (btn_menu){
+//           btn_menu.addEventListener('click', () => {
+//             const menu_items = document.querySelector('.menu_items')
+//             menu_items.classList.toggle('show')
+//           })
+//       }
+// })
 }
